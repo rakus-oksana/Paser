1c103c1ff2dd90603bcea139e0ac54d9e0143ac0
Oksana Rakus <rakus@ucu.edu.ua>
2018-10-15 23:29:29 +0300

    Initial commit

---
+++ src/main/java/Coordinates.java
@@ -0,0 +1,10 @@
+import lombok.ToString;
+import org.jsoup.select.Elements;
+
+import javax.xml.bind.Element;
+
+
+//TODO Create class that represents coordinates
+public class Coordinates {
+
+}
---
+++ src/main/java/WeatherForecaster.java
@@ -0,0 +1,21 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+/**
+ * Created by Victor on 04.10.2018.
+ */
+public class WeatherForecaster {
+
+
+    //TODO Apply forecast logic for city
+    public String forecast(City city){
+
+
+
+        return null;
+
+    }
+
+}
